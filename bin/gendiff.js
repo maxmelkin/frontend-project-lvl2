b1cb4e5d2d77be27c70814db7581753b6d7bf333
Max Melkin <maxim.melkin@ya.ru>
2021-06-05 22:51:37 +0300

    step 1.1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import program from 'commander';
+// const program = new program();
 program
   .description('Compares two configuration files and shows a difference.')  
   .version('0.1')
   
+  program.parse();
