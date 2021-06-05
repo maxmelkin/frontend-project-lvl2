d49e2c5ab5de70dd0d23e99010c6bee4b2c44293
Max Melkin <maxim.melkin@ya.ru>
2021-06-05 23:01:23 +0300

    step 2.0

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,9 @@
 import program from 'commander';
 // const program = new program();
 program
+  .version('0.1')  
   .description('Compares two configuration files and shows a difference.')  
-  .version('0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   
   program.parse();
